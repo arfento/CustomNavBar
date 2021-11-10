a895a786795b78ec86730699aa610bb59c95d847
Lakdar Arfento <arfento@gmail.com>
2021-11-10 16:54:46 +0700

    Initial commit

---
+++ app/src/main/java/com/ims/customnavbar/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ims.customnavbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
